dfb5af43eeb2e9704f265e17512cbc9addd65167
troyb9819 <troyb9819@gmail.com>
2024-08-29 09:23:50 -0500

    Add files via upload

---
+++ lab1a-1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	cout << "Hello world" << endl;
+	cout << "How much for some chicken" << endl;
+	cout << "How much for 3 pieces of candy" << endl;
+	cout << "How to be better at school" << endl;
+	return 0;
+
+}
