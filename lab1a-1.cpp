60ae02f41906f565cd8252d4ecc6c50506f7953e
setnormTJC <122695510+setnormTJC@users.noreply.github.com>
2024-08-29 09:58:41 -0500

    Update lab1a-1.cpp
    I am a homewrecker!

--- lab1a-1.cpp
+++ lab1a-1.cpp
@@ -1,15 +1,1 @@
-#include <iostream>
 
-using namespace std;
-
-
-int main()
-{
-	cout << "Hello world" << endl;
-	cout << "How much for some chicken" << endl;
-	cout << "How much for 3 pieces of candy" << endl;
-	cout << "How to be better at school" << endl;
-	return 0;
-	wow
-
-}
